8b0477cf9bcf8cfabcc0ba3796677cdd5dc32c15
Leif Johnson <leif@leifjohnson.net>
2014-08-17 21:10:59 -0500

    Add initial setup.py file, and flat data files.

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+import setuptools
+
+setuptools.setup(
+    name='depparse',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    author='Leif Johnson',
+    author_email='leif@leifjohnson.net',
+    description='dependency parsers for natural language text',
+    long_description=open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.rst')).read(),
+    license='MIT',
+    url='http://github.com/lmjohns3/py-depparse/',
+    keywords=('parsing '
+              'dependency-parsing '
+              'nlp '
+              'nlproc '
+              ),
+    data_files=[('depparse/data', ['data/mst-features.txt'])],
+    install_requires=['climate', 'scikit-learn'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+        ],
+    )
