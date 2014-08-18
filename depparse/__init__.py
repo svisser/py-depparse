11c355c82117e02eefd08054631628c0cccd8a50
Leif Johnson <leif@leifjohnson.net>
2014-08-17 21:22:25 -0500

    Add initial import of code from previous repo. Parallel learning still needs to be replaced with scikit-learn.

---
+++ depparse/__init__.py
@@ -0,0 +1,6 @@
+from .mst import Ruleset as MstRuleset
+from .mst import Parser as MstParser
+from .stack import Ruleset as StackRuleset
+from .stack import Parser as StackParser
+from .sentence import sentences_from_conll
+from .parser import Evaluator
