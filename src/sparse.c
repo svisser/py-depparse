8fac8a7cdc7adbbd25508ac402b808edb64d6a33
Leif Johnson <leif@leifjohnson.net>
2014-08-23 14:46:05 -0500

    Remove unused update function.

--- src/sparse.c
+++ src/sparse.c
@@ -31,49 +31,9 @@ dot(PyObject *self, PyObject *args)
     return (PyObject *) PyFloat_FromDouble(acc);
 }
 
-/* update({str: double} target, {str: double} source, double multiplier)
- *
- * Update a target set of sparse weights using a source set of sparse weights.
- * After this function has completed, the keys in target will be the union of
- * the keys in target and the keys in source, and the values in target will be
- * the sums of the values for the corresponding keys in target and source.
- * Source is not modified. Equivalent to the following Python:
- *
- * >>> for key, value in source.iteritems():
- * ...   target[key] = target.get(key, 0) + multiplier * value
- *
- * Both target and source should be dictionaries mapping strings (features) to
- * floats (weights). The multiplier should be a float.
- */
-static PyObject *
-update(PyObject *self, PyObject *args)
-{
-    PyObject *dst, *src, *k, *v, *x;
-    Py_ssize_t i = 0;
-    double mul, acc;
-    char *key;
-
-    if (!PyArg_ParseTuple(args, "OOd", &dst, &src, &mul))
-        return NULL;
-    while (PyDict_Next(src, &i, &k, &v)) {
-        if (!PyString_Check(k)) return NULL;
-        key = PyString_AsString(k);
-        if (!PyFloat_Check(v)) return NULL;
-        acc = mul * PyFloat_AS_DOUBLE(v);
-        x = PyDict_GetItemString(dst, key);
-        if (x != NULL)
-            acc += PyFloat_AS_DOUBLE(x);
-        PyDict_SetItemString(dst, key, PyFloat_FromDouble(acc));
-    }
-    Py_INCREF(Py_None);
-    return Py_None;
-}
-
 static PyMethodDef SparseMethods[] = {
     {"dot", dot, METH_VARARGS,
      "Sum a feature vector against a sparse weight dictionary."},
-    {"update", update, METH_VARARGS,
-     "Update one sparse weight dictionary using the values in another."},
     {NULL, NULL, 0, NULL} /* sentinel */
 };
 
