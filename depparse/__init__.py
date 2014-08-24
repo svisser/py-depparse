41677d519eae92617021f0ef4959a50c9ce6701b
Leif Johnson <leif@leifjohnson.net>
2014-08-24 09:38:06 -0500

    Import Voted Perceptron.

--- depparse/__init__.py
+++ depparse/__init__.py
@@ -1,6 +1,7 @@
 from .mst import Ruleset as MstRuleset
 from .mst import Parser as MstParser
+from .parser import Evaluator
+from .perceptron import VotedPerceptron
+from .sentence import sentences_from_conll
 from .stack import Ruleset as StackRuleset
 from .stack import Parser as StackParser
-from .sentence import sentences_from_conll
-from .parser import Evaluator
